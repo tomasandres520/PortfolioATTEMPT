a66ea7df74d66ad21a2de55313a7528f0540485e
PaulRomero24 <romeropaul24@gmail.com>
2025-09-15 16:59:33 -0300

    Clase 1-2-3 de Portfolio con Vue

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+//Desde la etiqueta "script" es lenguaje JS
+const nombre = "Paul Romero"
+</script>
+
+
+<template>
+  <!--Desde la etiqueta "TEMPLETE" para abajo es lenguaje HTML-->>
+  <h1>{{ nombre }}</h1>
+  <p>
+    Este es tu antiguo portfolio<a href="https://portfolio-individual.netlify.app/" target="_blank" rel="noopener">Portfolio</a>
+  </p>
+</template>
+
+<style scoped>
+/*Desde la etiqueta "STYLE" es lenguaje CSS*/
+
+</style>
